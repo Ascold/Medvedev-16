3959b5eb0ff295cb493be503f7f7db830992dcb8
Ascold <ascold@ex.ua>
2017-02-08 23:35:18 +0200

    dropdown is fixed (dislay none when inactive)

--- source-js/main.js
+++ source-js/main.js
@@ -1,3 +1,12 @@
 $('#navbar-button').on('click', function() {
+    if ($('#navbar-top').hasClass('display-off')) {
+            $('#navbar-top').removeClass('display-off')
+    } else {
+    setTimeout(function () {
+        $('#navbar-top').addClass('display-off')
+    }, 500)
+    }
+    setTimeout(function () {
     $('#navbar-top').toggleClass('active');
+    }, 1)
 });
